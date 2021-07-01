3a7461fba9800eddf85a6aeca122db1ec1b0685c
Tiago <ottiagos@gmail.com>
2021-06-30 21:38:18 -0300

    Pulling Categories from Magento Backend

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -5,10 +5,10 @@ import MainMenu from '../Main-menu/main-menu.component';
 
 class Header extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
 
-        this.state = {title: ''};
+        this.state = { title: '', menuItems: [] };
     }
 
     componentDidMount() {
@@ -16,23 +16,32 @@ class Header extends React.Component {
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState({title: result});
+                    this.setState({ title: result });
+                }
+            )
+
+        fetch("https://t14g.com/magento/rest/all/V1/categories?rootCategoryId=3&depth=1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let items = result.children_data;
+                    this.setState({ menuItems: items });
                 }
             )
     }
 
     render() {
         let title = this.state.title;
-        
+
         return (
             <div className="header">
                 <div className="container">
-                    
+
                     <span className="store-name">
-                    {title}
+                        {title}
                     </span>
 
-                    <MainMenu />
+                    <MainMenu items={this.state.menuItems} />
                 </div>
             </div>
         )
--- src/components/Main-menu/main-menu.component.jsx
+++ src/components/Main-menu/main-menu.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './main-menu.styles.scss';
 
-const MainMenu = () => {
+const MainMenu = ({ items = [] }) => {
 
     return (
         <ul className="main-menu">
-            <li className="menu-item">Home</li>
-            <li className="menu-item">Baralhos</li>
-            <li className="menu-item">Cordas</li>
-            <li className="menu-item">Outros</li>
-            <li className="menu-item">Contato</li>
+            {
+                items.map((item, index) => {
+                    return (
+                        <li key={index} className="menu-item">{item.name}</li>
+                    )
+                })
+            }
         </ul>
     );
 }
